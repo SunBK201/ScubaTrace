fde702b6df51f9c30c34984fae051edf466a91ea
SunBK201 <sunbk201gm@gmail.com>
2025-07-13 22:28:31 +0800

    fix: update CFile to use instance method for switching source/header files

--- scubatrace/file.py
+++ scubatrace/file.py
@@ -254,7 +254,7 @@ class CFile(File):
         """
         switch between the main source file (*.cpp) and header (*.h)
         """
-        uri = self.project.lsp.request_switch_source_header(self.relpath, self.uri)
+        uri = self.lsp.request_switch_source_header(self.relpath, self.uri)
         if len(uri) == 0:
             return None
         return self.project.files_uri.get(uri, None)
