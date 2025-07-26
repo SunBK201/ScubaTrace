2472ed26caf97e4087403ef19b24a1c38c350306
SunBK201 <sunbk201gm@gmail.com>
2025-07-26 22:02:41 +0800

    feat: enhance go support

--- scubatrace/go/language.py
+++ scubatrace/go/language.py
@@ -7,3 +7,95 @@ from ..language import Language
 class GO(Language):
     extensions = ["go"]
     tslanguage = TSLanguage(tsgo.language())
+
+    query_function = "(function_declaration)@name"
+    query_return = "(return_statement)@name"
+    query_call = "(call_expression)@name"
+    query_import_identifier = """
+        (import_declaration
+            (scoped_identifier
+                name: (identifier)@name
+            )
+        )
+        (import_declaration
+            (identifier)@name
+        )
+    """
+
+    query_package = "(package_clause)@name"
+    query_class = "(type_declaration)@name"
+
+    jump_statements = [
+        "break_statement",
+        "continue_statement",
+        "goto_statement",
+        "return_statement",
+    ]
+
+    block_statements = [
+        "if_statement",
+        "for_statement",
+        "expression_switch_statement",
+        "select_statement",
+        "communication_case",
+    ]
+
+    simple_statements = [
+        "var_declaration",
+        "const_declaration",
+        "assignment_statement",
+        "short_var_declaration",
+        "return_statement",
+        "break_statement",
+        "continue_statement",
+        "labeled_statement",
+        "go_statement",
+        "defer_statement",
+    ]
+
+    control_statements = [
+        "if_statement",
+        "for_statement",
+        "expression_switch_statement",
+        "select_statement",
+        "labeled_statement",
+    ]
+
+    loop_statements = [
+        "for_statement",
+    ]
+
+    @staticmethod
+    def query_left_value(text):
+        return f"""
+            (assignment_statement
+                left: (expression_list
+                    (identifier)@left
+                )
+                (#eq? @left "{text}")
+            )
+            (assignment_statement
+                left: (expression_list
+                    (selector_expression
+                        field: (field_identifier)@left
+                    )
+                    (#eq? @left "{text}")
+                )
+            )
+            (short_var_declaration
+                left: (expression_list)@left
+                (#eq? @left "{text}")
+            )
+            (var_declaration
+                (var_spec
+                    name: (identifier)@left
+                )
+                (#eq? @left "{text}")
+            )
+            (const_declaration
+                (const_spec
+                    name: (identifier)@left
+                )
+                (#eq? @left "{text}")
+            )
+        """
--- scubatrace/go/statement.py
+++ scubatrace/go/statement.py
@@ -16,4 +16,49 @@ if TYPE_CHECKING:
 class GoSimpleStatement(SimpleStatement): ...
 
 
-class GoBlockStatement(BlockStatement): ...
+class GoBlockStatement(BlockStatement):
+    def _build_statements(
+        self,
+        node: Node,
+        parent: BlockStatement | Function | File,
+    ) -> Generator[Statement, None, None]:
+        cursor = node.walk()
+        if cursor.node is not None:
+            if not cursor.goto_first_child():
+                yield from ()
+        while True:
+            assert cursor.node is not None
+            if self.language.is_simple_node(cursor.node):
+                yield GoSimpleStatement(cursor.node, parent)
+            elif self.language.is_block_node(cursor.node):
+                yield GoBlockStatement(cursor.node, parent)
+
+            if not cursor.goto_next_sibling():
+                break
+
+    @cached_property
+    def statements(self) -> list[Statement]:
+        stats = []
+        type = self.node.type
+        match type:
+            case "if_statement":
+                consequence_node = self.node.child_by_field_name("consequence")
+                if consequence_node is not None and consequence_node.type == "block":
+                    stats.extend(list(self._build_statements(consequence_node, self)))
+                elif consequence_node is not None:  # if () ...;
+                    stats.extend([GoSimpleStatement(consequence_node, self)])
+                else_clause_node = self.node.child_by_field_name("alternative")
+                if else_clause_node is not None:  # if () { ... } else if () { ... }
+                    stats.extend([GoBlockStatement(else_clause_node, self)])
+            case "for_statement":
+                body_node = self.node.child_by_field_name("body")
+                if body_node is not None and body_node.type in ["block"]:
+                    stats.extend(list(self._build_statements(body_node, self)))
+                elif body_node is not None:
+                    if self.language.is_simple_node(body_node):
+                        stats.extend([GoSimpleStatement(body_node, self)])
+                    elif self.language.is_block_node(body_node):
+                        stats.extend([GoBlockStatement(body_node, self)])
+            case _:
+                stats.extend(list(self._build_statements(self.node, self)))
+        return stats
