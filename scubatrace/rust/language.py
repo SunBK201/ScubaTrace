a30d446701c1be83083c01c00d85f92caa7a61f6
SunBK201 <sunbk201gm@gmail.com>
2025-07-27 16:24:26 +0800

    feat: enhance rust support

--- scubatrace/rust/language.py
+++ scubatrace/rust/language.py
@@ -7,3 +7,70 @@ from ..language import Language
 class RUST(Language):
     extensions = ["rs"]
     tslanguage = TSLanguage(tsrust.language())
+
+    query_function = "(function_item)@name"
+    query_return = "(return_expression)@name"
+    query_call = "(call_expression)@name"
+    query_import_identifier = """
+        (use_declaration
+        	argument: (identifier)@name
+        )
+        (use_declaration
+        	argument: (scoped_identifier
+            	name: (identifier)@name
+        )
+    """
+
+    query_class = "(struct_item)@name"
+
+    jump_statements = [
+        "break_expression",
+        "continue_expression",
+        "return_expression",
+    ]
+
+    block_statements = [
+        "if_expression",
+        "for_expression",
+        "while_expression",
+        "loop_expression",
+    ]
+
+    simple_statements = [
+        "expression_statement",
+        "let_declaration",
+        "assignment_expression",
+        "break_expression",
+        "continue_expression",
+        "return_expression",
+    ]
+
+    control_statements = [
+        "if_expression",
+        "for_expression",
+    ]
+
+    loop_statements = [
+        "for_expression",
+        "while_expression",
+        "loop_expression",
+    ]
+
+    @staticmethod
+    def query_left_value(text):
+        return f"""
+            (assignment_expression
+            	left: (identifier)@left
+                (#eq? @left "{text}")
+            )
+            (assignment_expression
+            	left: (field_expression
+                	field: (field_identifier)@left
+                )
+                (#eq? @left "{text}")
+            )
+            (let_declaration
+            	pattern: (identifier)@left
+                (#eq? @left "{text}")
+            )
+        """
--- scubatrace/rust/project.py
+++ scubatrace/rust/project.py
@@ -1,3 +1,5 @@
+from functools import cached_property
+
 from ..project import Project
 from . import language
 from .parser import RustParser
@@ -11,3 +13,10 @@ class RustProject(Project):
     @property
     def parser(self):
         return self._parser
+
+    @cached_property
+    def entry_point(self):
+        for func in self.functions:
+            if func.name == "main":
+                return func
+        return None
