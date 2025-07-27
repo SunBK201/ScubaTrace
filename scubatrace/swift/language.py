d6ab88267826b29aa4a283a74150398f4671fadc
SunBK201 <sunbk201gm@gmail.com>
2025-07-27 17:26:51 +0800

    feat: enhance swift support

--- scubatrace/swift/language.py
+++ scubatrace/swift/language.py
@@ -7,3 +7,60 @@ from ..language import Language
 class SWIFT(Language):
     extensions = ["swift"]
     tslanguage = TSLanguage(tsswift.language())
+
+    query_function = "(function_declaration)@name"
+    query_return = """
+    (control_transfer_statement)@name
+    (#eq? @left "return")
+    """
+    query_call = "(call_expression)@name"
+    query_import_identifier = """
+        (import_declaration
+            (identifier)@name
+        )
+    """
+
+    query_class = "(class_declaration)@name"
+
+    jump_statements = [
+        "control_transfer_statement",
+    ]
+
+    block_statements = [
+        "if_statement",
+        "for_statement",
+        "while_statement",
+        "repeat_while_statement",
+    ]
+
+    simple_statements = jump_statements + [
+        "import_declaration",
+        "property_declaration",
+        "call_expression",
+        "assignment",
+    ]
+
+    control_statements = block_statements
+
+    loop_statements = [
+        "for_statement",
+        "while_statement",
+        "repeat_while_statement",
+    ]
+
+    @staticmethod
+    def query_left_value(text):
+        return f"""
+            (assignment
+                target: (directly_assignable_expression
+                    (simple_identifier)@left
+                )
+                (#eq? @left "{text}")
+            )
+            (function_declaration
+                (parameter
+                    name: (simple_identifier)@left
+                )
+                (#eq? @left "{text}")
+            )
+        """
--- scubatrace/swift/statement.py
+++ scubatrace/swift/statement.py
@@ -16,4 +16,44 @@ if TYPE_CHECKING:
 class SwiftSimpleStatement(SimpleStatement): ...
 
 
-class SwiftBlockStatement(BlockStatement): ...
+class SwiftBlockStatement(BlockStatement):
+    def _build_statements(
+        self,
+        node: Node,
+        parent: BlockStatement | Function | File,
+    ) -> Generator[Statement, None, None]:
+        cursor = node.walk()
+        if cursor.node is not None:
+            if not cursor.goto_first_child():
+                yield from ()
+        while True:
+            assert cursor.node is not None
+            if self.language.is_simple_node(cursor.node):
+                yield SwiftSimpleStatement(cursor.node, parent)
+            elif self.language.is_block_node(cursor.node):
+                yield SwiftBlockStatement(cursor.node, parent)
+
+            if not cursor.goto_next_sibling():
+                break
+
+    @cached_property
+    def statements(self) -> list[Statement]:
+        stats = []
+        type = self.node.type
+        match type:
+            case "if_statement":
+                consequence_node = self.file.parser.query_oneshot(
+                    self.node, "(statements)@name"
+                )
+                if consequence_node is not None:
+                    stats.extend(list(self._build_statements(consequence_node, self)))
+                else_clause_node = self.node.child_by_field_name("if_statement")
+                if else_clause_node is not None:
+                    stats.extend([SwiftBlockStatement(else_clause_node, self)])
+            case "for_statement" | "while_statement" | "repeat_while_statement":
+                body_node = self.node.child_by_field_name("statements")
+                if body_node is not None:
+                    stats.extend(list(self._build_statements(body_node, self)))
+            case _:
+                stats.extend(list(self._build_statements(self.node, self)))
+        return stats
